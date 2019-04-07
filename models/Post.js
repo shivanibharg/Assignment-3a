8636d12cd1cf2c69eb79eeb49bfcc449b9dbdf08
shivanibharg <shivanibharg@gmail.com>
2019-04-07 16:14:53 -0700

    posts and register

--- models/Post.js
+++ models/Post.js
@@ -17,36 +17,36 @@ const PostSchema = new Schema({
   avatar: {
     type: String
   },
-  likes: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      }
-    }
-  ],
-  comments: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      },
-      text: {
-        type: String,
-        required: true
-      },
-      name: {
-        type: String
-      },
-      avatar: {
-        type: String
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      }
-    }
-  ],
+  // likes: [
+  //   {
+  //     user: {
+  //       type: Schema.Types.ObjectId,
+  //       ref: 'users'
+  //     }
+  //   }
+  // ],
+  // comments: [
+  //   {
+  //     user: {
+  //       type: Schema.Types.ObjectId,
+  //       ref: 'users'
+  //     },
+  //     text: {
+  //       type: String,
+  //       required: true
+  //     },
+  //     name: {
+  //       type: String
+  //     },
+  //     avatar: {
+  //       type: String
+  //     },
+  //     date: {
+  //       type: Date,
+  //       default: Date.now
+  //     }
+  //   }
+  // ],
   date: {
     type: Date,
     default: Date.now
